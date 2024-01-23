5cfce430cf1d5dd1c53f4e33b3689dd56777f144
Nihan Nur Gönenç <nnurgonenc@gmail.com>
2024-01-24 01:27:50 +0300

    Notepad Uygulaması

--- Notepad/src/Main.java
+++ Notepad/src/Main.java
@@ -3,24 +3,34 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-
         Scanner input = new Scanner(System.in);
-        System.out.print("Notunuzu yazınız: "); //Kullanıcıdan not alımı
-        String text = input.nextLine();
 
         try {
-            FileWriter fWriter = new FileWriter("notlar.txt", true); //eski notlar silinmeden yenileri eklenecek
-            PrintWriter pWriter = new PrintWriter(fWriter);
-            pWriter.println(text);
-            pWriter.close();
-
             FileReader fReader = new FileReader("notlar.txt");
             BufferedReader bReader = new BufferedReader(fReader);
             String readText = bReader.readLine();
+
+            if(readText != null) { // Başlangıçta not boş değilse tüm notu gösterir
+                System.out.println("Eski notlarınız: " + readText);
+            }
+
+            fReader.close();
             bReader.close();
+
+            FileWriter fWriter = new FileWriter("notlar.txt", true); //eski notlar silinmeden yenileri eklenecek
+            PrintWriter pWriter = new PrintWriter(fWriter);
+
+            System.out.print("Eklemek istediğiniz notları yazınız: "); //Kullanıcıdan not alımı
+            String writeText = input.nextLine();
+            pWriter.print(writeText);
+            fWriter.close();
+            pWriter.close();
 
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+
+
+
     }
 }
